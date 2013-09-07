d6c39ab0ea7cb47bb7f20d201600eab14c21fe10
David <carsmetic.oc@gmail.com>
2013-09-07 11:23:36 +0700

    About us static page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,5 @@
 class HomeController < ApplicationController
   def index
+    session[:current_page] = "home"
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def active_link(page)
+    if !session[:current_page].blank? && session[:current_page] == page
+      "active"
+    else
+      ""
+    end
+  end
 end

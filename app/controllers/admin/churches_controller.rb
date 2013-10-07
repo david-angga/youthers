1c7c95f0c2e019272b50fd74f5ec8348b411a4bb
David <carsmetic.oc@gmail.com>
2013-10-07 21:14:31 +0700

    update & delete churches

--- app/controllers/admin/churches_controller.rb
+++ app/controllers/admin/churches_controller.rb
@@ -24,6 +24,52 @@ class Admin::ChurchesController < ApplicationController
     end
   end
 
+  def edit
+    @church = Church.find(params[:id])
+
+    if @church.nil?
+      flash[:error] = "Gereja tidak dapat ditemukan."
+
+      redirect_to admin_churches_url
+    end
+  end
+
+  def update
+    @church = Church.find(params[:id])
+
+    if !@church.nil?
+      if @church.update_attributes(church_param)
+        flash[:notice] = "Gereja berhasil diupdate."
+
+        redirect_to admin_churches_url
+      else
+        flash[:error] = "Gereja gagal diupdate."
+
+        redirect_to edit_admin_church_url(@church.id)
+      end
+    else
+      flash[:error] = "Gereja tidak dapat ditemukan."
+
+      redirect_to admin_churches_url
+    end
+  end
+
+  def destroy
+    @church = Church.find(params[:id])
+
+    if @church.nil?
+      flash[:error] = "Gereja tidak dapat ditemukan."
+
+      redirect_to admin_churches_url
+    else
+      @church.destroy
+
+      flash[:notice] = "Gereja berhasil dihapus."
+
+      redirect_to admin_churches_url
+    end
+  end
+
   protected
   def set_active_page
     session[:current_page] = "churches"

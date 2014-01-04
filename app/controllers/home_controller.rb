22e2ab2a861a06f8838df9de42209a63115d672c
David <carsmetic.oc@gmail.com>
2014-01-04 21:22:23 +0700

    Devotions RSS

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,24 @@
 class HomeController < ApplicationController
+  before_filter :get_devotions
+
   def index
     session[:current_page] = "home"
     @events = Event.find(:all, :order => "event_date DESC", :limit => 3)
+  end
+
+  protected
+
+  def get_devotions
+    require 'rss'
+    require 'open-uri'
+
+    url = 'http://getmorestrength.org/rssfeed/'
+    open(url) do |rss|
+      @feed = RSS::Parser.parse(rss)
+      #puts "Title: #{feed.channel.title}"
+      #feed.items.each do |item|
+      #  puts "Item: #{item.title}"
+      #end
+    end
   end
 end
